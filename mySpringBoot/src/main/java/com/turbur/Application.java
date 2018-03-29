e131ebf2ef8704885b194c536a9e51587fc1fd07
xulizheng <122750351@qq.com>
2018-03-29 13:07:18 +0800

    springboot框架练习

--- mySpringBoot/src/main/java/com/turbur/Application.java
+++ mySpringBoot/src/main/java/com/turbur/Application.java
@@ -3,7 +3,7 @@ package com.turbur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //@Configuration
