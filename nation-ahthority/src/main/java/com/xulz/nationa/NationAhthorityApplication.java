c78196b0d1f178050933f210fd1d857af6da8dd2
xulz <122750351@qq.com>
2019-09-19 11:58:22 +0800

    新建获取国家鉴权项目

---
+++ nation-ahthority/src/main/java/com/xulz/nationa/NationAhthorityApplication.java
@@ -0,0 +1,13 @@
+package com.xulz.nationa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationAhthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationAhthorityApplication.class, args);
+    }
+
+}
